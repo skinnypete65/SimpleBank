a5dfb2c8b3cf3d0047ba37a5e11941e167c2a7c1
skinnypete65 <skinnypetetskipe@gmail.com>
2023-06-20 17:40:05 +0300

    tests for transfer

--- db/sqlc/entry_test.go
+++ db/sqlc/entry_test.go
@@ -19,6 +19,12 @@ func createRandomEntry(t *testing.T, account Account) Entry {
 	require.NoError(t, err)
 	require.NotEmpty(t, entry)
 
+	require.Equal(t, arg.AccountID, entry.AccountID)
+	require.Equal(t, arg.Amount, entry.Amount)
+
+	require.NotZero(t, entry.ID)
+	require.NotZero(t, entry.CreatedAt)
+
 	return entry
 }
 
